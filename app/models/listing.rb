cd169878fd4e2a1a02d61667044c9d68e2922fca
Goose39 <Goose39.Dev@gmail.com>
2016-03-22 13:24:34 +0200

    User+Listing Validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ActiveRecord::Base
 has_attached_file :image, styles: { medium: "200x>", thumb: "100x>" }, default_url: "default.gif"
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+validates :Name, :description, :price, presence: true
+validates :price, numericality: {greater_than: 0}
+validates_attachment_presence :image
 end
